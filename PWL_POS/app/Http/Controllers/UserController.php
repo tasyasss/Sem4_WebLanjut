b829667afd1fc6391ed68db26ef9ef4fac923081
tasyasss <tasyasakina0703@gmail.com>
2025-03-14 19:31:21 +0700

    js 4 - prak 2.1 - retrieving single models

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,19 +10,27 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan eloquent model
-        $data = [
-            'username' => 'Manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-            'level_id' => 2
-        ];
-        UserModel::create($data); 
-        // UserModel::insert($data); //menambahkan data ke m_user
-
-        // coba akses model UserModel
-        $user = UserModel::all(); //ambil semua data m_user
+        // $user = UserModel::find(1);
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id', 1);
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
+
+        // // tambah data user dengan eloquent model
+        // $data = [
+        //     'username' => 'Manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        // ];
+        // UserModel::create($data); 
+        // // UserModel::insert($data); //menambahkan data ke m_user
+
+        // // coba akses model UserModel
+        // $user = UserModel::all(); //ambil semua data m_user
+        // return view('user', ['data' => $user]);
     }
 }
 
