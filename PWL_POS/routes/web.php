4a12d68c11ff9bb51a4f71d1a0392befca131bb6
tasyasss <tasyasakina0703@gmail.com>
2025-03-25 22:44:05 +0700

    js 6 - prak 3 - Modal Ajax Hapus Data (Data User)

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -38,6 +38,8 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);     // menampilkan form perubahan data user AJAX
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);     // menyimpan perubahan data user AJAX
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); // menampilkan form confirm delete AJAX
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // menghapus data user AJAX
 });
 
 Route::group(['prefix' => 'level'], function () {
