b03b051b08f90087277139ad3dcaeaf7c1a9f7ac
tasyasss <tasyasakina0703@gmail.com>
2025-05-17 01:37:42 +0700

    js 8 - prak 2 - Export Data ke Excel

--- PWL_POS/app/Http/Controllers/BarangController.php
+++ PWL_POS/app/Http/Controllers/BarangController.php
@@ -357,4 +357,61 @@ class BarangController extends Controller
         'message' => 'Request tidak valid'
     ]);
 }
+
+    public function export_excel()
+    {
+        // ambil data yang akan di export
+        $barang = BarangModel::select(
+            'kategori_id', 
+            'barang_kode', 
+            'barang_nama', 
+            'harga_beli', 
+            'harga_jual')
+            ->orderBy('kategori_id')->with('kategori')->get();
+
+        // load library excel
+        $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet(); //ambil sheet yang aktif
+
+        $sheet->setCellValue('A1', 'No');
+        $sheet->setCellValue('B1', 'Kode Barang');
+        $sheet->setCellValue('C1', 'Nama Barang');
+        $sheet->setCellValue('D1', 'Harga Beli');
+        $sheet->setCellValue('E1', 'Harga Jual');
+        $sheet->setCellValue('F1', 'Kategori');
+
+        $sheet->getStyle('A1:F1')->getFont()->setBold(true); // bold header
+
+        $no = 1;        // no data dari 1
+        $baris = 2;     // baris data dari baris 2
+        foreach ($barang as $key => $value) {
+            $sheet->setCellValue('A' . $baris, $no);
+            $sheet->setCellValue('B' . $baris, $value->barang_kode);
+            $sheet->setCellValue('C' . $baris, $value->barang_nama);
+            $sheet->setCellValue('D' . $baris, $value->harga_beli);
+            $sheet->setCellValue('E' . $baris, $value->harga_jual);
+            $sheet->setCellValue('F' . $baris, $value->kategori->kategori_nama);
+            $baris++;
+            $no++;
+        }
+
+        foreach (range('A', 'F') as $columnID) {
+            $sheet->getColumnDimension($columnID)->setAutoSize(true); // set a
+        }
+
+        $sheet->setTitle('Data Barang');
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $filename = 'Data Barang ' . date('Y-m-d H:i:s') . '.xlsx';
+
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="' . $filename . '"');
+        header('Cache-Control: max-age=0');
+        header('Cache-Control: max-age=1');
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+        header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+        header('Cache-Control: cache, must-revalidate');
+        header('Pragma: no-cache');
+        $writer->save('php://output');
+        exit;
+    }
 }
--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -116,6 +116,7 @@ Route::middleware('auth')->group(function () { // route di bawah ini hanya bisa 
             Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
             Route::get('/import',[BarangController::class, 'import']); // ajax form upload excel
             Route::post('/import_ajax', [BarangController::class, 'import_ajax']); //ajax import excel
+            Route::get('/export_excel',[BarangController::class, 'export_excel']); // export excel
         });
     });
     
