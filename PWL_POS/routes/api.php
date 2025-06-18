917933245ddcd59b40943ffa4a80789ce455a0c8
tasyasss <tasyasakina0703@gmail.com>
2025-06-18 21:27:53 +0700

    js 10 - tugas - implementasi crud api

--- PWL_POS/routes/api.php
+++ PWL_POS/routes/api.php
@@ -31,3 +31,21 @@ Route::post('levels', [\App\Http\Controllers\Api\LevelController::class, 'store'
 Route::get('levels/{level}', [\App\Http\Controllers\Api\LevelController::class, 'show']);
 Route::put('levels/{level}', [\App\Http\Controllers\Api\LevelController::class, 'update']);
 Route::delete('levels/{level}', [\App\Http\Controllers\Api\LevelController::class, 'destroy']);
+
+Route::get('users', [\App\Http\Controllers\Api\UserController::class, 'index']);
+Route::post('users', [\App\Http\Controllers\Api\UserController::class, 'store']);
+Route::get('users/{user}', [\App\Http\Controllers\Api\UserController::class, 'show']);
+Route::put('users/{user}', [\App\Http\Controllers\Api\UserController::class, 'update']);
+Route::delete('users/{user}', [\App\Http\Controllers\Api\UserController::class, 'destroy']);
+
+Route::get('barangs', [\App\Http\Controllers\Api\BarangController::class, 'index']);
+Route::post('barangs', [\App\Http\Controllers\Api\BarangController::class, 'store']);
+Route::get('barangs/{barang}', [\App\Http\Controllers\Api\BarangController::class, 'show']);
+Route::put('barangs/{barang}', [\App\Http\Controllers\Api\BarangController::class, 'update']);
+Route::delete('barangs/{barang}', [\App\Http\Controllers\Api\BarangController::class, 'destroy']);
+
+Route::get('kategoris', [\App\Http\Controllers\Api\KategoriController::class, 'index']);
+Route::post('kategoris', [\App\Http\Controllers\Api\KategoriController::class, 'store']);
+Route::get('kategoris/{kategori}', [\App\Http\Controllers\Api\KategoriController::class, 'show']);
+Route::put('kategoris/{kategori}', [\App\Http\Controllers\Api\KategoriController::class, 'update']);
+Route::delete('kategoris/{kategori}', [\App\Http\Controllers\Api\KategoriController::class, 'destroy']);
